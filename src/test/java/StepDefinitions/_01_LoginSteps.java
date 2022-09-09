50b43f2531b0973e11c232405a4b61f1de2fd058
ozank <ozankarabiyik@gmail.com>
2022-09-09 16:32:07 +0300

    User-1 bir kac kucuk degisiklik

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -18,11 +18,6 @@ public class _01_LoginSteps {
 
     @When("Enter username and password and click login button")
     public void enterUsernameAndPasswordAndClickLoginButton() {
-        // waitleri var
-//        dc.username.sendKeys("richfield.edu");
-//        dc.password.sendKeys("Richfield2020!");
-//        dc.loginButton.click();
-        // 1 .aşama
         dc.findAndSend("username","richfield.edu");
         dc.findAndSend("password","Richfield2020!");
         dc.findAndClick("loginButton");
@@ -30,9 +25,6 @@ public class _01_LoginSteps {
 
     @Then("User should login successfuly")
     public void userShouldLoginSuccessfuly() {
-//        dc.waitUntilVisible(dc.dashboard);
-//        Assert.assertTrue(dc.dashboard.getText().contains("Dashboard"));
-
         dc.findAndContainsText("dashboard","Dashboard");
         dc.findAndClick("acceptCookies");
     }
